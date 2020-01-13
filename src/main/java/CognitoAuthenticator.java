c27e9bbfcd34e4cd83572cd775de76204b523e7c
Jonas Ferreira <jonas.mcferreira@gmail.com>
2020-01-13 13:35:10 +0000

    none: configured aws sdk

--- src/main/java/CognitoAuthenticator.java
+++ src/main/java/CognitoAuthenticator.java
@@ -1,7 +1,12 @@
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
+
 public class CognitoAuthenticator {
 
     public static void main(String[] args){
-
+        CognitoIdentityProviderClient cognitoUserPoolClient = CognitoIdentityProviderClient.builder()
+          .region(Region.of("eu-central-1"))
+          .build();
 
 
     }
